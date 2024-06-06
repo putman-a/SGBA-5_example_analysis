052fc3d48054ede2b4c49c99c8b342a6cd96f211
A Putman <andrew.putman@ontariotechu.net>
2024-06-06 15:12:53 -0230

    re-added negative examples

--- SGBA-5_sim_SGBA.R
+++ SGBA-5_sim_SGBA.R
@@ -72,7 +72,7 @@ grol_dens  # view plot
 ### by biological sex example ---------------------------------------------
 
 # density plot
-pos_cont_sex_dens <- ggplot(sim_data, aes(x = outcome_num_pos_sex, fill = bio_sex)) + 
+neg_cont_sex_dens <- ggplot(sim_data, aes(x = outcome_num_neg_sex, fill = bio_sex)) + 
   geom_density(alpha = 0.7) +
   scale_fill_manual(values = c('forestgreen', 'skyblue')) +
   labs(title = 'Density Plot of Simulated Continuous Variable by Biological Sex (n=30)') +
@@ -80,22 +80,22 @@ pos_cont_sex_dens <- ggplot(sim_data, aes(x = outcome_num_pos_sex, fill = bio_se
   ylab('Response Density') +
   theme_classic() +
   theme(legend.position = 'bottom', text = element_text(family = 'serif'))
-pos_cont_sex_dens  # view plot
+neg_cont_sex_dens  # view plot
 
 # summary stats
-pos_cont_sex_sum <- sim_data %>% group_by(bio_sex) %>% 
+neg_cont_sex_sum <- sim_data %>% group_by(bio_sex) %>% 
   summarise(
     n = n(), 
-    `mean continuous` = round(mean(outcome_num_pos_sex),1),
-    `SD continuous` = round(sd(outcome_num_pos_sex),2),
-    `median continuous` = round(median(outcome_num_pos_sex),0),
-    `IQR continuous` = round(IQR(outcome_num_pos_sex),)
+    `mean continuous` = round(mean(outcome_num_neg_sex),1),
+    `SD continuous` = round(sd(outcome_num_neg_sex),2),
+    `median continuous` = round(median(outcome_num_neg_sex),0),
+    `IQR continuous` = round(IQR(outcome_num_neg_sex),)
   ) %>% 
   rename(`biological sex` = bio_sex) 
-pos_cont_sex_sum   # view stats
+neg_cont_sex_sum   # view stats
 
 # t test
-con_sex_ttest <- t.test(outcome_num_pos_sex ~ bio_sex, data = sim_data)
+con_sex_ttest <- t.test(outcome_num_neg_sex ~ bio_sex, data = sim_data)
 con_sex_ttest   # view test results
 
 
@@ -353,3 +353,294 @@ pos_binary_grole_dens  # view plot
 # t test
 pos_binary_grole_ttest <- t.test(gen_role ~ outcome_bin_pos_grol, data = sim_data)
 pos_binary_grole_ttest   # view test results
+
+
+# SGBA negative examples --------------------------------------------------
+
+## continuous variable ----------------------------------------------------
+
+### by biological sex example ---------------------------------------------
+
+# density plot
+neg_cont_sex_dens <- ggplot(sim_data, aes(x = outcome_num_neg, fill = bio_sex)) + 
+  geom_density(alpha = 0.7) +
+  scale_fill_manual(values = c('forestgreen', 'skyblue')) +
+  labs(title = 'Density Plot of Simulated Continuous Variable by Biological Sex (n=30)') +
+  xlab('Continuous Outcome') + 
+  ylab('Response Density') +
+  theme_classic() +
+  theme(legend.position = 'bottom', text = element_text(family = 'serif'))
+neg_cont_sex_dens  # view plot
+
+# summary stats
+neg_cont_sex_sum <- sim_data %>% group_by(bio_sex) %>% 
+  summarise(
+    n = n(), 
+    `mean continuous` = round(mean(outcome_num_neg),1),
+    `SD continuous` = round(sd(outcome_num_neg),2),
+    `median continuous` = round(median(outcome_num_neg),0),
+    `IQR continuous` = round(IQR(outcome_num_neg),)
+  ) %>% 
+  rename(`biological sex` = bio_sex) 
+neg_cont_sex_sum   # view stats
+
+# t test
+con_sex_ttest <- t.test(outcome_num_neg ~ bio_sex, data = sim_data)
+con_sex_ttest   # view test results
+
+
+### by gender identity example -------------------------------------------
+
+# scatter plot
+neg_cont_gi_scatter <- ggplot(
+  data = sim_data, aes(x = gen_id, y = outcome_num_neg)
+) + 
+  geom_jitter(size = 3, colour = 'forestgreen') + 
+  # labs(title = 'Scatterplot of Simulated Numerical Outcome\n& Gender Identity (n=30)') +
+  ylab('Continuous Outcome') +
+  xlab('Feminine - Masculine Continuum') +
+  gender_plot_theme
+neg_cont_gi_scatter  # view plot
+
+# 2D density plot
+neg_cont_gi_dens2d <- plot_dens_2D(
+  sim_data, gen_id, outcome_num_neg
+) +
+  #  labs(title = '2D Density of Simulated Numerical\nOutcome & Gender Identity (n=30)') +
+  ylab('Continuous Outcome') +
+  xlab('Feminine - Masculine Continuum') +
+  gender_plot_theme
+neg_cont_gi_dens2d  # view plot
+
+# Pearson's r
+neg_cont_gi_cor <- cor.test(
+  sim_data$outcome_num_neg, 
+  sim_data$gen_id, 
+  method = 'pearson'
+)
+neg_cont_gi_cor   # view test results
+
+
+### by gender role example -----------------------------------------------
+
+# scatter plot
+neg_cont_role_scatter <- ggplot(
+  sim_data, aes(x = gen_role, y = outcome_num_neg)
+) + 
+  geom_jitter(size = 3, colour = 'forestgreen') + 
+  #  labs(title = 'Scatterplot of Simulated Numerical Outcome\n& Gender Role') +
+  ylab('Simuluated Continuous Outcome') +
+  xlab('Feminine - Masculine Continuum') +
+  gender_plot_theme
+neg_cont_role_scatter  # view plot
+
+# 2D density plot
+neg_cont_role_dens2d <- plot_dens_2D(
+  sim_data, gen_role, outcome_num_neg
+) +
+  #  labs(title = '2D Density of Simulated Numerical\nOutcome & Gender Role (n=30)') +
+  ylab('Simuluated Continuous Outcome') +
+  xlab('Feminine - Masculine Continuum') +
+  gender_plot_theme
+neg_cont_role_dens2d  # view plot
+
+# Pearson's r
+neg_cont_grol_cor <- cor.test(
+  sim_data$outcome_num_neg,
+  sim_data$gen_role,
+  method='pearson'
+)
+neg_cont_grol_cor  # view test results
+
+
+## ordinal variable -------------------------------------------------------
+
+### by biological sex example ---------------------------------------------
+
+# bar plot
+neg_ord_sex_bar <- ggplot(
+  sim_data, aes(x = outcome_ord_neg, fill = bio_sex)
+) +
+  geom_bar() + 
+  facet_wrap(~ bio_sex) +
+  scale_fill_manual(values = c('forestgreen', 'skyblue')) +
+  labs(title = 'Bar Plot of Simulated Likert Outcome and Sex Assigned\nat Birth(n=30)') +
+  xlab('Simulated Likert Scale Variable') +
+  theme(panel.grid = element_blank(),panel.background = element_blank(), 
+        legend.position = 'none', text = element_text(family = 'serif'))
+neg_ord_sex_bar  # view plot
+
+# summary stats
+neg_ord_sex_sum <- sim_data %>% group_by(bio_sex) %>% 
+  summarise(
+    n = n(), 
+    `median Likert` = round(median(as.numeric(outcome_ord_neg)),0),
+    `IQR Likert` = round(IQR(as.numeric(outcome_ord_neg)),)
+  ) %>% 
+  rename(`biological sex` = bio_sex) 
+neg_ord_sex_sum   # view stats
+
+# chi-square test
+neg_ord_sex_chi <- chisq.test(
+  x = sim_data$outcome_ord_neg, 
+  y = sim_data$bio_sex
+)
+neg_ord_sex_chi   # view test results
+
+
+### by gender identity example ---------------------------------------------
+
+# jitter
+neg_ord_gi_jit <- ggplot(
+  sim_data, aes(x = gen_id, y = outcome_ord_neg)
+) +
+  geom_jitter(
+    aes(colour = outcome_ord_neg, alpha = 0.7), size = 4, height = 0.1
+  ) +
+  # labs(title = 'Scatterplot of Simulated Likert Outcome\n& Gender Identity (n=30)') +
+  xlab('Feminine - Masculine Continuum') +
+  ylab('Simuluated Likert Outcome') +
+  geom_hline(
+    yintercept = c(1.5, 2.5, 3.5, 4.5, 5.5, 6.5),
+    linetype='dashed', colour='grey'
+  ) +
+  gender_plot_theme
+neg_ord_gi_jit  # view plot
+
+# 2D Density
+neg_ord_gi_den <- plot_dens_2D(
+  sim_data, gen_id, as.numeric(outcome_ord_neg)
+) +
+  scale_y_continuous(
+    breaks = c(1,2,3,4,5,6,7), labels = c(1,2,3,4,5,6,7), limits = c(1,7)
+  ) +
+  # labs(title = '2D Density of Simulated Likert Outcome\n& Gender Identity (n=30)') +
+  xlab('Feminine - Masculine Continuum') +
+  ylab('Simuluated Likert Outcome') + 
+  geom_hline(yintercept = c(1.5, 2.5, 3.5, 4.5, 5.5, 6.5)) +
+  gender_plot_theme
+neg_ord_gi_den  # view plot
+
+## Pearson's r
+neg_ord_gi_cor <- cor.test(
+  as.numeric(sim_data$outcome_ord_neg), 
+  sim_data$gen_id, 
+  method = 'pearson'
+)
+neg_ord_gi_cor   # view test results
+
+
+### by gender role example -------------------------------------------------
+
+# jitter
+neg_ord_gr_jit <- ggplot(
+  sim_data, aes(x = gen_role, y = outcome_ord_neg)
+) + 
+  geom_jitter(
+    aes(colour = outcome_ord_neg, alpha = 0.7), size = 4, height = 0.1
+  ) +
+  # labs(title = 'Scatterplot of Simulated Likert Outcome\n& Gender Role (n=30)') +
+  xlab('Feminine - Masculine Continuum') +
+  ylab('Simuluated Likert Outcome') +
+  geom_hline(
+    yintercept = c(1.5, 2.5, 3.5, 4.5, 5.5, 6.5), 
+    linetype='dashed', colour='grey'
+  ) +
+  gender_plot_theme
+neg_ord_gr_jit  # view plot
+
+# 2D Density
+neg_ord_gr_den <- plot_dens_2D(
+  sim_data, gen_role, as.numeric(outcome_ord_neg)
+) +
+  scale_y_continuous(
+    breaks = c(1,2,3,4,5,6,7), labels = c(1,2,3,4,5,6,7), limits = c(1,7)
+  ) +
+  # labs(title = '2D Density of Simulated Likert Outcome\n& Gender Role (n=30)') +
+  xlab('Feminine - Masculine Continuum') +
+  ylab('Simuluated Likert Outcome') + 
+  geom_hline(yintercept = c(1.5, 2.5, 3.5, 4.5, 5.5, 6.5)) +
+  gender_plot_theme
+neg_ord_gr_den  # view plot        
+
+# Pearson's r
+neg_ord_gr_cor <- cor.test(
+  as.numeric(sim_data$outcome_ord_neg), 
+  sim_data$gen_role, 
+  method='pearson'
+)
+neg_ord_gr_cor   # view test results
+
+
+## binary variable -------------------------------------------------------
+
+### by biological sex example ---------------------------------------------
+
+# bar plot
+neg_binary_sex_bar <- ggplot(
+  sim_data, aes(x = outcome_bin_neg, fill = bio_sex)
+) +
+  geom_bar() + 
+  facet_wrap(~ bio_sex) +
+  scale_fill_manual(values = c('forestgreen', 'skyblue')) +
+  labs(title = 'Bar Plot of Simulated Binary Outcome and Sex Assigned\nat Birth(n=30)') +
+  xlab('Simulated Binary Variable') +
+  theme(panel.grid = element_blank(),panel.background = element_blank(), 
+        legend.position = 'none', text = element_text(family = 'serif'))
+neg_binary_sex_bar  # view plot
+
+# frequency table
+neg_binary_sex_freq <- table(sim_data$outcome_bin_neg, sim_data$bio_sex)
+neg_binary_sex_freq   # view stats
+
+# chi-square test
+neg_binary_sex_chi <- chisq.test(sim_data$outcome_bin_neg, sim_data$bio_sex)
+neg_binary_sex_chi   # view test results
+
+
+### by gender identity example ---------------------------------------------
+
+# bar plot
+neg_binary_gi_dens <- ggplot(
+  sim_data, aes(x = gen_id, fill = outcome_bin_neg)
+) +
+  geom_density(alpha = 0.7) + 
+  scale_fill_manual(values = c('forestgreen', 'skyblue')) +
+  labs(title = 'Density Plots of Simulated Binary Outcome and Gender Identity\n(n=30)') +
+  xlab('Feminine - Masculine Continuum') +
+  theme_classic() +
+  theme(
+    text = element_text(family = 'serif'),
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank()
+  )
+neg_binary_gi_dens  # view plot
+
+# t test
+neg_binary_gi_ttest <- t.test(gen_id ~ outcome_bin_neg, data = sim_data)
+neg_binary_gi_ttest   # view test results
+
+
+### by gender roles example ------------------------------------------------
+
+# bar plot
+neg_binary_grole_dens <- ggplot(
+  sim_data, aes(x = gen_role, fill = outcome_bin_neg)
+) +
+  geom_density(alpha = 0.7) + 
+  # facet_wrap(~ sim_binary) +
+  scale_fill_manual(values = c('forestgreen', 'skyblue')) +
+  labs(title = 'Density Plots of Simulated Binary Outcome and Gender Roles\n(n=30)') +
+  xlab('Feminine - Masculine Continuum') +
+  theme_classic() +
+  theme(
+    text = element_text(family = 'serif'),
+    axis.ticks.x = element_blank(),
+    axis.text.x = element_blank()
+  )
+neg_binary_grole_dens  # view plot
+
+# t test
+neg_binary_grole_ttest <- t.test(gen_role ~ outcome_bin_neg, data = sim_data)
+neg_binary_grole_ttest   # view test results
+
